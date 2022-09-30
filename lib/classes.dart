8eee7e9ac7f3c124b9eaf84d776ee47bfbf454af
Kodknackarn-sys <bengrundis@gmail.com>
2022-09-30 16:15:32 +0200

    Hej hej hej

--- lib/classes.dart
+++ lib/classes.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class TodoState extends ChangeNotifier {
   final List<TodoListState> _list = [];
 
+  String nyckel = "391016ed-e7f4-46be-85dc-2a612623cc9e";
+  String hemsida = "https://todoapp-api.apps.k8s.gu.se/todos";
+
   List<TodoListState> get list => _list;
 
   String _filterBy = 'All';
